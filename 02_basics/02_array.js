cdab4899f9944ae3aa571caaeea0a3f9de8aab39
Tushika2024 <tushikacoderbsr@gmail.com>
2025-12-23 13:00:01 +0530

    Update 02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -9,12 +9,10 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
-
+const all_new_heros = [...marvel_heros, ...dc_heros]//spread operator
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
-
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array);
 
