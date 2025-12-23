60e03d2f10fb4f2aa71f3236824748796f95cd80
Tushika2024 <tushikacoderbsr@gmail.com>
2025-12-23 14:41:55 +0530

    Update 04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,10 +1,9 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+const tinderUser = new Object()  //singelton 
+const tinderUser = {} //non singleton
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
-
 // console.log(tinderUser);
 
 const regularUser = {
@@ -16,16 +15,14 @@ const regularUser = {
         }
     }
 }
-
 // console.log(regularUser.fullname.userfullname.firstname);
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
-
 // const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
 
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
 
@@ -48,7 +45,7 @@ const users = [
 users[1].email
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
+// console.log(Object.keys(tinderUser));  in form of array of keys
 // console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser));
 
@@ -60,17 +57,17 @@ const course = {
     price: "999",
     courseInstructor: "hitesh"
 }
-
-// course.courseInstructor
-
-const {courseInstructor: instructor} = course
-
+//use of curly braces to access property- object destructuring
+// course.courseInstructor  very tedious to write such long name
+const {courseInstructor} = course
 // console.log(courseInstructor);
-console.log(instructor);
+const {courseInstructor: instructor} = course
+// console.log(instructor);
 
+api gives response in json format
 // {
 //     "name": "hitesh",
-//     "coursename": "js in hindi",
+//     "coursename": "js in hindi",    
 //     "price": "free"
 // }
 
