ebf2e98f9c7ea5f1101372eb0e5bb5ff72d64c1e
Tushika2024 <tushikacoderbsr@gmail.com>
2025-12-24 14:52:06 +0530

    Update six.js

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -1,27 +1,25 @@
-// const coding = ["js", "ruby", "java", "python", "cpp"]
+const coding = ["js", "ruby", "java", "python", "cpp"]
+const values = coding.forEach( (item) => {
+    //console.log(item);
+    return item
+} )
+// console.log(values);=>undefined=>foreach loop dosent return anything
 
 
-// const values = coding.forEach( (item) => {
-//     //console.log(item);
-//     return item
-// } )
-
-// console.log(values);
-
+//filter
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const newNums = myNums.filter( (num) => {
+    return num > 4
+} )
 
-// const newNums = myNums.filter( (num) => {
-//     return num > 4
-// } )
 
-// const newNums = []
 
-// myNums.forEach( (num) => {
-//     if (num > 4) {
-//         newNums.push(num)
-//     }
-// } )
-
+const newNums = []//above functionality using forEachloop
+myNums.forEach( (num) => {
+    if (num > 4) {
+        newNums.push(num)
+    }
+} )
 // console.log(newNums);
 
 
