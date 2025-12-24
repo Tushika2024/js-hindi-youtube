b757aaaa7e447ae0f13b9536d8db852c47ff0918
Tushika2024 <tushikacoderbsr@gmail.com>
2025-12-24 13:29:24 +0530

    Update four.js

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,26 +1,28 @@
+//for in ->key 
+
+
 const myObject = {
     js: 'javascript',
     cpp: 'C++',
     rb: "ruby",
     swift: "swift by apple"
 }
-
 for (const key in myObject) {
     //console.log(`${key} shortcut is for ${myObject[key]}`);
 }
 
-const programming = ["js", "rb", "py", "java", "cpp"]
 
+const programming = ["js", "rb", "py", "java", "cpp"]
 for (const key in programming) {
     //console.log(programming[key]);
 }
 
-// const map = new Map()
-// map.set('IN', "India")
-// map.set('USA', "United States of America")
-// map.set('Fr', "France")
-// map.set('IN', "India")
 
-// for (const key in map) {
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+for (const key in map) {=>not printing anything
 //     console.log(key);
 // }
