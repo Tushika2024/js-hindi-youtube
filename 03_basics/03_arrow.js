f6a53f16b4c222340f5d8cb7def01f64db832477
Tushika2024 <tushikacoderbsr@gmail.com>
2025-12-24 11:14:46 +0530

    Update 03_arrow.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,3 +1,4 @@
+-------this important concept in normal and arrow functions read thoroughly-------
 const user = {
     username: "hitesh",
     price: 999,
@@ -8,45 +9,38 @@ const user = {
     }
 
 }
-
 // user.welcomeMessage()
 // user.username = "sam"
 // user.welcomeMessage()
 
-// console.log(this);
+// console.log(this);=>in node empty object but in browser console 
 
-// function chai(){
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
 
-// chai()
-
-// const chai = function () {
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
-
-const chai =  () => {
+function chai(){
     let username = "hitesh"
-    console.log(this);
+    console.log(this.username);=>undefined this not referring to function chai
+}
+// chai()
+const chai = function () {
+    let username = "hitesh"
+    console.log(this.username);
 }
 
 
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this); //=>empty object
+}
 // chai()
 
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
-// }
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
 
-// const addTwo = (num1, num2) =>  num1 + num2
-
-// const addTwo = (num1, num2) => ( num1 + num2 )
-
+const addTwo = (num1, num2) =>  num1 + num2 //implicit return
+const addTwo = (num1, num2) => ( num1 + num2 )
 const addTwo = (num1, num2) => ({username: "hitesh"})
-
-
-console.log(addTwo(3, 4))
+// console.log(addTwo(3, 4))
 
 
 // const myArray = [2, 5, 3, 7, 8]
